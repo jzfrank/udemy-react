6b4fd2fd15c8ec56e8f9a51d0864367448715562
jzfrank <frankjinzhang@gmail.com>
2022-06-22 20:30:56 +0200

    .

--- class-based-components/src/components/UserFinder.js
+++ class-based-components/src/components/UserFinder.js
@@ -4,6 +4,7 @@ import { Fragment, useState, useEffect } from "react";
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
 import UsersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary";
 
 const DUMMY_USERS = [
   { id: "u1", name: "Max" },
@@ -49,7 +50,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }
