6a178c08aa469964fca3980440696f5741d98ce0
jzfrank <frankjinzhang@gmail.com>
2022-06-25 10:02:22 +0200

    added styles

--- food-order-app-http/src/components/Cart/Checkout.js
+++ food-order-app-http/src/components/Cart/Checkout.js
@@ -6,13 +6,13 @@ const Checkout = (props) => {
   };
 
   return (
-    <form onSubmit={confirmHandler}>
+    <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" />
       </div>
       <div className={classes.control}>
-        <label htmlFor="Street">Street</label>
+        <label htmlFor="street">Street</label>
         <input type="text" id="street" />
       </div>
       <div className={classes.control}>
@@ -23,10 +23,12 @@ const Checkout = (props) => {
         <label htmlFor="city">City</label>
         <input type="text" id="city" />
       </div>
-      <button type="button" onClick={props.onCancel}>
-        Cancel
-      </button>
-      <button>Confirm</button>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
