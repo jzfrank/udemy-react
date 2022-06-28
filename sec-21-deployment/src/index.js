5a5163603469049cdb4cf795babdedbc4cceaa45
jzfrank <frankjinzhang@gmail.com>
2022-06-28 08:45:07 +0200

    lazy loading

--- sec-21-deployment/src/index.js
+++ sec-21-deployment/src/index.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
@@ -6,7 +8,9 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
